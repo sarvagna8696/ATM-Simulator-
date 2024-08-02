7c0be774b5db4db032dbe97fa486eb2735e58e53
sarvagna8696 <sarvagnabingi@gmail.com>
2024-08-02 14:44:52 +0530

    Create conn.java

---
+++ conn.java
@@ -0,0 +1,25 @@
+package bank;
+
+import java.sql.*;
+
+public class conn {
+
+    Connection c;
+    Statement s;
+
+    public conn() {
+
+        try {
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql:///bank", "root", "8333806103");
+            s = c.createStatement();
+
+        } catch (Exception e) {
+
+            System.out.println(e);
+        }
+
+    }
+
+}
